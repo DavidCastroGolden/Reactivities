c781a190a88af502c2865584fe8e0206ef684233
David Castro Ochoa <dcastro@devbot.com>
2021-01-18 11:53:18 -0600

    client-app react project added

---
+++ client-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// npx create-react-app client-app --use-npm --template typescript
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reactivities</h1>
+    </div>
+  );
+}
+
+export default App;
